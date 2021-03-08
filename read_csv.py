f931fe969c6cf0badf9c0b80393763db13e45421
Sandro Wehrhahn <sandro.wehrhahn@uni-oldenburg.de>
2021-03-08 15:34:48 +0100

    Review

--- read_csv.py
+++ read_csv.py
@@ -1,12 +1,7 @@
-import numpy as np
 import pandas as pd
-import csv
 
-#path="../Ecosense/bikeride-gps-0aad2d83b1bef753c6223006240d4b730ffc755e.csv"
 
-def readCsvFiles(path):
-    data = (pd.read_csv(path, sep=';'))
+def dataframe_from_csv_file(path):
+    data = pd.read_csv(path, sep=';')
+    data = data.stack().str.replace(',', '.').unstack()
     return data
-
-
-#print(readCsvFiles("Ecosense/bikeride-gps-5d49d8db2c913a4631c46f77380b67052def385e.csv"))
