a47f455898fa8375df22ba4428dafb8fb39a4c2a
Dennis <dennis.schulte@uni-oldenburg.de>
2021-03-08 12:30:47 +0100

    Kommas sind nicht nötig

--- distance_calc.py
+++ distance_calc.py
@@ -15,9 +15,9 @@ def calc_distance(route):
     co2 = ""
     for index, row in route.iterrows():
         if co1 == "":
-            co1 = str(row['latitude']) + ", " + str(row['longitude']) #critical: the order has to be lat, lon
+            co1 = str(row['latitude']) + " " + str(row['longitude']) #critical: the order has to be lat, lon
         else:
-            co2 = str(row['latitude']) + ", " + str(row['longitude'])
+            co2 = str(row['latitude']) + " " + str(row['longitude'])
             dist = dist + distance.distance(Point(co1), Point(co2)).m
             co1 = co2
     return dist
