6ba04faacf4bb5aff6eb5b7daf1f9cb1e25c1dc0
Dennis <dennis.schulte@uni-oldenburg.de>
2021-03-09 11:26:08 +0100

    avg, min, max von speed werden ausgegeben

---
+++ speed_calc.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def calc_speed(df):
+    avg = np.mean(df['speed'])
+    maximum = np.max(df['speed'])
+    minimum = np.min(df['speed'])
+
+    return [avg,maximum,minimum]
