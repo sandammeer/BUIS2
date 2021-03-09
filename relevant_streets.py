c53cd3a47911e3fed2919034b42f291a477daff0
Dennis <dennis.schulte@uni-oldenburg.de>
2021-03-09 18:14:00 +0100

    effizienter und mehr straßen

--- relevant_streets.py
+++ relevant_streets.py
@@ -1,33 +1,27 @@
 import glob
-
-import geocoder as geocoder
+from datetime import datetime
 import pandas as pd
-
 from geopy.geocoders import Nominatim
-
-from geopy import distance
-
 from geopy import Point
 
 uni_routes = []
-uni_streets = ['Ammerländer Heerstraße', 'Uhlhornsweg', 'Haarenfeld', 'Schützenweg', 'Artillerieweg', 'Wechloyer Weg', 'Quellenweg']
+uni_streets = ['Ammerländer Heerstraße', 'Uhlhornsweg', 'Haarenfeld', 'Schützenweg', 'Artillerieweg', 'Wechloyer Weg',
+               'Quellenweg','Ofener Str.','Heiligengeistwall','Theaterwall','Carl-von-Ossietzky-Straße','Im Technologiepark',
+               'Tuchtweg','Binsenstraße','Prinzessinweg','Drögen-Hasen-Weg','Grotepool','Küpkersweg','Pophankenweg','Gabelsbergerweg','Franz-Poppe-Straße','Westerstraße','Rummelweg','Zeughausstraße']
 geolocator = Nominatim(user_agent="test_app")
 def uni_street_in_route(path):
 
     data = pd.read_csv(path, sep=';')
     values = data.values
 
-    '''
-    for index in range(len(values)):
-        print(type(index))
-        item = values[index] if values % 2 == 0 else values[len(values) - index]
-        print(type(item))
-        co1 = str(item['latitude']) + ", " + str(item['longitude'])
 
-    '''
     for index, row in data.iterrows():
-        if index % 2 != 0:
+        if index % 10 == 1:
             row = data.iloc[len(values) - index]
+        elif index % 10 == 0:
+            row = row
+        else:
+            continue
         if index >= len(values)/2:
             print('durch', index, len(values))
             return False
@@ -45,15 +39,17 @@ def uni_street_in_route(path):
 
 def search_all_routes():
     counter = 0
+    print(datetime.now())
     for item in glob.glob("Ecosense\*.csv"):
         print(item)
         if uni_street_in_route(item):
             uni_routes.append(item)
         counter += 1
-        if counter > 2:
+        if counter > 5:
             print('done')
             return uni_routes
     return uni_routes
 
 print(search_all_routes())
+print(datetime.now())
 #uni_street_in_route("Ecosense/bikeride-gps-2dde982a4f5acf1a4c06c5d004cefc75cfeed173.csv")
