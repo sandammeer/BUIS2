a1cc666ff0f3e1c1c4d83f6c5af85c9e91994db0
Sandro Wehrhahn <sandro.wehrhahn@uni-oldenburg.de>
2021-03-09 15:38:22 +0100

    Finish writing data to index file

--- write_index.py
+++ write_index.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import os 
+import importlib
+
+
+reader = importlib.import_module("read_csv")
+index_filename = "Index.csv"
 
 
 def write_to_index(filename: str, 
@@ -7,31 +12,35 @@ def write_to_index(filename: str,
                    distance: float, 
                    speed: float,
                    relevant: bool):
-    data = {
-        "Filename": [filename],
-        "Mean_Street_Quality": [mean_street_quality],
-        "Distance": [distance],
-        "Speed": [speed],
-        "Relevant": [relevant]
-    }
+    
+    new_data = {"Filename": filename, 
+                "Mean_Street_Quality": mean_street_quality, 
+                "Distance": distance, 
+                "Speed": speed, 
+                "Relevant": relevant}
+    original_data = {"Filename": [filename], 
+                    "Mean_Street_Quality": [mean_street_quality], 
+                    "Distance": [distance], 
+                    "Speed": [speed], 
+                    "Relevant": [relevant]}
+    
+    if os.path.exists(index_filename):
+        df = pd.read_csv(index_filename, index_col=0)
+        df = pd.concat([df, pd.DataFrame.from_dict(original_data)], ignore_index=True, sort=False)
+    else:
+        df = pd.DataFrame(original_data) 
 
-    df = None
-    if os.path.exists("Index.csv"):
-        # read csv
-        # append data to df
-        print("todo")
-    else:     
-        df = pd.DataFrame(data)
-
-    df.to_csv("Index.csv")
+    df.to_csv(index_filename)
 
 
-if __name__ == "__main__":
-    print("test")
-    write_to_index(
-        filename="test.csv",
-        mean_street_quality=10.0,
-        distance= 10.0,
-        speed=10.0,
-        relevant=True
-    )
+# For Review
+
+# if __name__ == "__main__":
+    
+#     write_to_index(
+#         filename="test.csv",
+#         mean_street_quality=10.0,
+#         distance= 10.0,
+#         speed=10.0,
+#         relevant=True
+#     )
